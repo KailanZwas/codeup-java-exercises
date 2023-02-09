4456e7dfee54b9e779e1d4a6ee2c55f46bf134a0
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2023-02-09 11:40:39 -0600

    day 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 4;
+        System.out.println(myFavoriteNumber);
+        String myString = "I dont like java";
+        System.out.println("I dont like java");
+       Float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        Object var;
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
     }
 }
+
