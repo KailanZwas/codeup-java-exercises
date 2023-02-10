5be07a54748982c780c5e2b4a27b67186ecf4f60
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2023-02-10 11:23:01 -0600

    do while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,21 @@
 public class ControlFlowExercises {
-//    todo: Loop Basics
-public static void main(String[] args){
+    //    todo: Loop Basics
+    public static void main(String[] args) {
 
-    int i = 5;
-    while (i <= 15){
-        System.out.println("i is " + i);
-        i++;
-    }
-}
+//    int i = 5;
+//    while (i <= 15){
+//        System.out.println("i is " + i);
+//        i++;
+//    }
+//        // TODO: Do WHile
+//        int i = 2;
+//        do {
+//            System.out.println("i is " + i);
+//            i+=2;
+//        } while (i <= 100);
+
+
+
+
+   }
 }
