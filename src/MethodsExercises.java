fb8f355dbe40c52afd6e2147c8d5135bc338b0db
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2023-02-13 11:20:14 -0600

    q-1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,2 +1,23 @@
 public class MethodsExercises {
+
+        public static int addMethod ( int a, int b){
+            return a + b;
+        }
+        public static int subMethod (int a, int b){
+            return a - b;
+        }
+        public static int divideMethod (int a , int b){
+            return a / b;
+        }
+        public static int multiplyMethod (int a , int b){
+            return a * b;
+        }
+
+//    logging
+    public static void main(String[] args) {
+        System.out.println(addMethod(5, 3));
+        System.out.println(subMethod(5,2));
+        System.out.println(divideMethod(5,5));
+        System.out.println(multiplyMethod(5,5));
+    }
 }
